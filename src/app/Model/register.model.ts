a7f85d8ce5291e686e146398d2ebaedd2b0e14e9
salamancadaniel <df.parras1@uniandes.edu.co>
2020-11-28 21:41:21 -0500

    register

--- src/app/Model/register.model.ts
+++ src/app/Model/register.model.ts
@@ -1,9 +1,11 @@
 export class register{
     name:any;
+    lastName:any;
     email: any;
     password: any;
     country: any;
     city: any;
     phone: any;
-
+    document: any;
+    documentType: any;
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -21,29 +21,49 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 register(form: NgForm){
-  if(form.value.name != '' && form.value.email != '' && form.value.password != '' && form.value.country != '' && form.value.city != '' && form.value.phone != '' && form.value.location != ''){
+  if(form.value.name.trim() != '' && 
+     form.value.email.trim() != '' && 
+     form.value.password.trim() != '' && 
+     form.value.country.trim() != '' && 
+     form.value.city.trim() != '' && 
+     form.value.phone.trim() != '' && 
+     form.value.document.trim() != '' && 
+     form.value.documentType.trim() != '' &&
+     form.value.lastName.trim() != ''
+     ){
     let registerobj = new register();//Armar el objeto register para enviarlo como parámetro al servicio
     registerobj.name = form.value.name;
+    registerobj.lastName = form.value.lastName;
     registerobj.email = form.value.email;
     registerobj.password = form.value.password;
     registerobj.country = form.value.country;
     registerobj.city = form.value.city;
     registerobj.phone = form.value.phone;
+    registerobj.document = form.value.document;
+    registerobj.documentType = form.value.documentType;
     
 
+    this.mainservice.setUser(registerobj).subscribe( data => {
 
-
-    if(this.mainservice.setUser(registerobj)){
-      this.router.navigate(['']);
-
-      this.toastr.success('Su usuario ha sido creado!', 'Felicidades', {
-        positionClass: 'toast-bottom-right',
-        progressBar: true,
-        progressAnimation: 'decreasing',
-        timeOut: 5000
-      });
-    }
-    
+      console.log(data)
+      if(data['message'] === 'account created'){
+        this.router.navigate(['/login']);
+  
+        this.toastr.success('Su usuario ha sido creado!', 'Felicidades', {
+          positionClass: 'toast-bottom-right',
+          progressBar: true,
+          progressAnimation: 'decreasing',
+          timeOut: 5000
+        });
+      }else{
+        this.toastr.error('Ha ocurrido un error, por favor intente nuevamente!', 'Felicidades', {
+          positionClass: 'toast-bottom-right',
+          progressBar: true,
+          progressAnimation: 'decreasing',
+          timeOut: 5000
+        });
+      }
+    });
   }
 }
 }
